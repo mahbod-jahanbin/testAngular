f18b4164d0541e0a2581260be09f3e94bb77cd34
jahanbin <jahanbin@sgpco.com>
2025-04-14 17:15:39 +0330

    Enables two-way data binding for family name
    Improves user experience on the home page by enabling two-way data binding for the family name input field.
    Also, change the event from keypress to input in name field.
    Imports FormsModule in auth, landing modules and home component.

--- src/app/modules/landing/components/home/home.component.ts
+++ src/app/modules/landing/components/home/home.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import myStr from '../../models/my-str';
-import { LoginComponent } from '../../../auth/components/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
 })
 export class HomeComponent {
   title = 'Home';
   public FullName_home = new myStr('mahbod', 'jahanbin');
+
+
 
   public handleChangeName(change: Event) {
     const myInput = change.target as HTMLInputElement;
@@ -19,7 +21,6 @@ export class HomeComponent {
   }
   public handleChangeFamily(change: Event) {
     const myInput = change.target as HTMLInputElement;
-
     this.FullName_home.Family = myInput.value;
   }
 }
--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from '../../app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [
+  
+   
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HomeComponent
+  ],
 })
 export class LandingModule {}
