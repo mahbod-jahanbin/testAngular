eedc4274db023ddc39e2789bb508b52b60b71ccf
jahanbin <jahanbin@sgpco.com>
2025-04-14 15:18:20 +0330

    Adds Bootstrap and jQuery dependencies
    Imports Bootstrap CSS for styling and includes jQuery and Bootstrap JS files for enhanced UI interactivity.
    Also, modifies the home component to use Bootstrap classes and adds a button.

--- src/app/modules/landing/components/home/home.component.ts
+++ src/app/modules/landing/components/home/home.component.ts
@@ -7,17 +7,17 @@ import { LoginComponent } from '../../../auth/components/login/login.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [],
 })
 export class HomeComponent {
   title = 'Home';
   public FullName_home = new myStr('mahbod', 'jahanbin');
 
- public handleChangeName(change: Event) {
+  public handleChangeName(change: Event) {
     const myInput = change.target as HTMLInputElement;
     this.FullName_home.Name = myInput.value;
   }
- public handleChangeFamily(change: Event) {
+  public handleChangeFamily(change: Event) {
     const myInput = change.target as HTMLInputElement;
 
     this.FullName_home.Family = myInput.value;
