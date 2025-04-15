58a800f21b7395bd4117cb30ed80240df8053409
jahanbin <jahanbin@sgpco.com>
2025-04-15 16:39:43 +0330

    Enhances home component with data binding
    Refactors the home component to use a view model for data binding, allowing for dynamic updates to name and family properties.
    Also, it adds a list of users

--- src/app/modules/landing/components/home/home.component.ts
+++ src/app/modules/landing/components/home/home.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
 import myStr from '../../models/my-str';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
 })
 export class HomeComponent {
   title = 'Home';
-  public FullName_home = new myStr('mahbod', 'jahanbin');
 
-
+  public viewModel = new myStr('Mahbod', 'Jahanbin');
 
   public handleChangeName(change: Event) {
     const myInput = change.target as HTMLInputElement;
-    this.FullName_home.Name = myInput.value;
+    this.viewModel.Name = myInput.value;
   }
-  public handleChangeFamily(change: Event) {
-    const myInput = change.target as HTMLInputElement;
-    this.FullName_home.Family = myInput.value;
-  }
+
+  users:string[]=['ali','hamid','reza','maya','Rasul'];
+
+  // public handleChangeFamily(change: Event) {
+  //   const myInput = change.target as HTMLInputElement;
+  //   this.FullName_home.Family = myInput.value;
+  // }
 }
